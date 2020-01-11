cf1d0dc1b7a5fa901bf5a2c2336ee6a9de8e5a7e
thle01 <lethehien18@gmail.com>
2020-01-11 12:53:10 -0800

    go
    go

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,15 +2,31 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function RenderPartner() {
-    
+
+function RenderPartner({partner}) {
+    if (partner) {
+        return (
+        <React.Fragment>
+            <Media object src={partner.image} alt={partner.name} width="150" />
+            <Media body className='ml-5 mb-4'>
+                <Media heading>
+                    {partner.name}
+                </Media>
+                {partner.description}
+            </Media>
+        </React.Fragment>
+        );
+    }
+    return <div />;
 }
 
 function About(props) {
 
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+            <Media tag="li" key={partner.id}>
+                <RenderPartner partner={partner}/>
+            </Media>
         );
     });
 
