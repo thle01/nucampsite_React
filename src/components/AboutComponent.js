96e5549b67c68f0057078e87d6fec2e143b4e56a
thle01 <lethehien18@gmail.com>
2020-01-11 12:26:38 -0800

    go
    go

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+function RenderPartner() {
+    
+}
 
 function About(props) {
 
