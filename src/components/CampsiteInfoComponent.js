7447d4c80f36b4c4b006a80df20ac01eceeb7a46
thle01 <lethehien18@gmail.com>
2020-01-18 13:09:00 -0800

    go
    go

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,111 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const required = val => val && val.length;
+const minLength = len => val => val && (val.length >= len);
+const maxLength = len => val => !val || (val.length <= len);
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        console.log("Current state is: " + JSON.stringify(values));
+        alert("Current state is: " + JSON.stringify(values));
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+
+                <Button outline onClick={this.toggleModal} >
+                    <i className="fa fa-pencil fa-lg" /> Submit Comment
+                </Button>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div class="form-group">
+                                <Label htmlFor="rating" md={2}>Rating</Label>
+                                <Col md={10}>
+                                    <Control.select
+                                        model=".rating"
+                                        id="rating"
+                                        name="rating"
+                                        className="form-control">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </Control.select>
+                                </Col>
+                            </div>
+                            <div class="form-group">
+                                <Label htmlFor="author" md={3}> Your name</Label>
+                                <Col md={10}>
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be 2 characters or more',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </div>
+                            <div class="form-group">
+                                <Label htmlFor="text" md={2}>Comment</Label>
+                                <Col md={10}>
+                                    <Control.textarea model=".text" id="text" name="text"
+                                        className="form-control"
+                                        rows={6}
+                                    />
+                                </Col>
+                            </div>
+                            <div class="form-group">
+                                <Col md={10}>
+                                    <Button type="submit" value="submit" color="primary"> Submit</Button>
+                                </Col>
+                            </div>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}
 
 function RenderCampsite({ campsite }) {
     return (
@@ -30,6 +135,7 @@ function RenderComments({ comments }) {
                         </div>
                     );
                 })}
+                <CommentForm />
             </div>
         );
     }
@@ -44,7 +150,7 @@ function CampsiteInfo(props) {
                     <div className="col">
                         <Breadcrumb>
                             <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-        <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
                         </Breadcrumb>
                         <h2>{props.campsite.name}</h2>
                         <hr />
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -8,7 +8,6 @@ class Header extends Component {
     constructor(props) {
         super(props);
 
-        
         this.state = {
           isNavOpen: false,
           isModalOpen: false
